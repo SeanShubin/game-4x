12fc1edb21bd46718f27901252afab2510731203
Sean Shubin <sshubin@cj.com>
2023-02-18 17:35:01 -0800

    updated names

--- game/src/test/kotlin/com/seanshubin/game_4x/game/SampleGameTest.kt
+++ game/src/test/kotlin/com/seanshubin/game_4x/game/SampleGameTest.kt
@@ -8,15 +8,15 @@ class SampleGameTest {
     @Test
     fun colonize(){
         val planetResourcesA = Resources(listOf(
-            Resource("food", quantity = 4, rate = 4),
-            Resource("material", quantity = 2, rate = 7),
-            Resource("energy", quantity = 6, rate = 5)
+            Resource(Names.FOOD, quantity = 4, rate = 4),
+            Resource(Names.MATERIAL, quantity = 2, rate = 7),
+            Resource(Names.ENERGY, quantity = 6, rate = 5)
         ))
         val planetA = Planet("Planet A", size = 10, planetResourcesA).addOrbital("colonizer")
         val planetResourcesB = Resources(listOf(
-            Resource("food", quantity = 2, rate = 3),
-            Resource("material", quantity = 8, rate = 6),
-            Resource("energy", quantity = 3, rate = 4)
+            Resource(Names.FOOD, quantity = 2, rate = 3),
+            Resource(Names.MATERIAL, quantity = 8, rate = 6),
+            Resource(Names.ENERGY, quantity = 3, rate = 4)
         ))
         val planetB = Planet("Planet B", size = 6, planetResourcesB)
         val strategy:Strategy = StrategyImpl()
